358248fdca6abb0ec57fde96ef5171344cbc3e30
M.boneh <maryamboneh@gmail.com>
2021-02-09 22:43:47 +0330

    fix bugs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
+from functools import partial
 
 num = ''
 temp_num = ''
@@ -20,16 +21,16 @@ class main(QMainWindow):
         self.ui.show()
         self.ui.textEdit.setText('0')
 
-        self.ui.btn_zero.clicked.connect(self.enter_num_0)
-        self.ui.btn_one.clicked.connect(self.enter_num_1)
-        self.ui.btn_two.clicked.connect(self.enter_num_2)
-        self.ui.btn_three.clicked.connect(self.enter_num_3)
-        self.ui.btn_four.clicked.connect(self.enter_num_4)
-        self.ui.btn_five.clicked.connect(self.enter_num_5)
-        self.ui.btn_six.clicked.connect(self.enter_num_6)
-        self.ui.btn_seven.clicked.connect(self.enter_num_7)
-        self.ui.btn_eight.clicked.connect(self.enter_num_8)
-        self.ui.btn_nine.clicked.connect(self.enter_num_9)
+        self.ui.btn_zero.clicked.connect(partial(self.enter_num, 0))
+        self.ui.btn_one.clicked.connect(partial(self.enter_num, 1))
+        self.ui.btn_two.clicked.connect(partial(self.enter_num, 2))
+        self.ui.btn_three.clicked.connect(partial(self.enter_num, 3))
+        self.ui.btn_four.clicked.connect(partial(self.enter_num, 4))
+        self.ui.btn_five.clicked.connect(partial(self.enter_num, 5))
+        self.ui.btn_six.clicked.connect(partial(self.enter_num, 6))
+        self.ui.btn_seven.clicked.connect(partial(self.enter_num, 7))
+        self.ui.btn_eight.clicked.connect(partial(self.enter_num, 8))
+        self.ui.btn_nine.clicked.connect(partial(self.enter_num, 9))
 
         self.ui.btn_mul.clicked.connect(self.enter_mul)
         self.ui.btn_divide.clicked.connect(self.enter_divid)
@@ -43,135 +44,10 @@ class main(QMainWindow):
         self.ui.btn_clear.clicked.connect(self.enter_clear)
         self.ui.btn_dote.clicked.connect(self.enter_dote)
 
-        #----------------------- STYLES -------------------------#
-
-        self.ui.btn_zero.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_one.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_two.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_three.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_four.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_five.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_six.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_seven.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_eight.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-        self.ui.btn_nine.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: white")
-
-        self.ui.btn_mul.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_divide.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_sub.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_plus.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_tan.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_cos.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_sin.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_equal.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_clear.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-        self.ui.btn_dote.setStyleSheet(
-            " border-radius: 25px ; font-size : 16px ; margin: 2px ; background-color: #fdfdfd ; color: orange ;")
-
-        self.ui.textEdit.setStyleSheet(
-            " background-color: #f1f1f1 ; border: 0px ;font-size : 18px ; padding: 15px")
-
-        #----------------------- END STYLES -------------------------#
-
-    def enter_num_1(self):
+    def enter_num(self, n):
         global num
         global temp_num
-        num = num + '1'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_2(self):
-        global num
-        num = num + '2'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_3(self):
-        global num
-        num = num + '3'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_4(self):
-        global num
-        num = num + '4'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_5(self):
-        global num
-        num = num + '5'
-
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_6(self):
-        global num
-        num = num + '6'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_7(self):
-        global num
-        num = num + '7'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_8(self):
-        global num
-        global temp_num
-        global opr
-        num = num + '8'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_9(self):
-        global num
-        num = num + '9'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
-
-    def enter_num_0(self):
-        global num
-        if len(num) > 0 or temp_num:
-            num = num + '0'
+        num = num + str(n)
         if temp_num:
             self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
         else:
@@ -183,9 +59,12 @@ class main(QMainWindow):
         global opr
         if (num or temp_num):
             if opr:
-                temp_num = self.amaliat()
-                num = ''
-                self.ui.textEdit.setText((str(temp_num) + "*"))
+                if num:
+                    temp_num = str(self.amaliat())
+                    num = ''
+                    self.ui.textEdit.setText((str(temp_num) + "*"))
+                else:
+                    self.ui.textEdit.setText((str(temp_num) + "*"))
 
             else:
                 self.ui.textEdit.setText((str(num) + "*"))
@@ -199,9 +78,12 @@ class main(QMainWindow):
         global opr
         if (num or temp_num):
             if opr:
-                temp_num = self.amaliat()
-                num = ''
-                self.ui.textEdit.setText((str(temp_num) + "/"))
+                if num:
+                    temp_num = str(self.amaliat())
+                    num = ''
+                    self.ui.textEdit.setText((str(temp_num) + "/"))
+                else:
+                    self.ui.textEdit.setText((str(temp_num) + "/"))
 
             else:
                 self.ui.textEdit.setText((str(num) + "/"))
@@ -215,9 +97,12 @@ class main(QMainWindow):
         global opr
         if (num or temp_num):
             if opr:
-                temp_num = self.amaliat()
-                num = ''
-                self.ui.textEdit.setText((str(temp_num) + "-"))
+                if num:
+                    temp_num = str(self.amaliat())
+                    num = ''
+                    self.ui.textEdit.setText((str(temp_num) + "-"))
+                else:
+                    self.ui.textEdit.setText((str(temp_num) + "-"))
 
             else:
                 self.ui.textEdit.setText((str(num) + "-"))
@@ -229,12 +114,15 @@ class main(QMainWindow):
         global temp_num
         global num
         global opr
+
         if (num or temp_num):
             if opr:
-                temp_num = self.amaliat()
-                num = ''
-                self.ui.textEdit.setText((str(temp_num) + "+"))
-
+                if num:
+                    temp_num = str(self.amaliat())
+                    num = ''
+                    self.ui.textEdit.setText((str(temp_num) + "+"))
+                else:
+                    self.ui.textEdit.setText((str(temp_num) + "+"))
             else:
                 self.ui.textEdit.setText((str(num) + "+"))
                 temp_num = num
@@ -267,44 +155,54 @@ class main(QMainWindow):
 
     def enter_dote(self):
         global num
-        num = num + '.'
-        if temp_num:
-            self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
-        else:
-            self.ui.textEdit.setText(str(num))
+        if (num.count('.') == 0):
+            num = num + '.'
+            if temp_num:
+                self.ui.textEdit.setText(str(temp_num) + (opr) + str(num))
+            else:
+                self.ui.textEdit.setText(str(num))
 
     def enter_equal(self):
         global temp_num
         global num
         global opr
+        temp_num = float("{:.2f}".format(float(temp_num)))
+        num = float("{:.2f}".format(float(num)))
         if opr == '*':
-            self.ui.textEdit.setText(str((float(temp_num)) * (float(num))))
-            temp_num = (float(num)) * (float(temp_num))
+            self.ui.textEdit.setText(str(temp_num * num))
+            temp_num = num * temp_num
         elif opr == '/':
-            self.ui.textEdit.setText(str((float(temp_num)) / (float(num))))
-            temp_num = (float(num)) / (float(temp_num))
+            self.ui.textEdit.setText(str(temp_num / num))
+            temp_num = num / temp_num
         elif opr == '-':
-            self.ui.textEdit.setText(str((float(temp_num)) - (float(num))))
-            temp_num = (float(num)) - (float(temp_num))
+            self.ui.textEdit.setText(str(temp_num - num))
+            temp_num = num - temp_num
         elif opr == '+':
-            self.ui.textEdit.setText(str(float(temp_num) + (float(num))))
-            temp_num = (float(num)) + (float(temp_num))
+            self.ui.textEdit.setText(str(temp_num + num))
+            temp_num = num + temp_num
         else:
             self.ui.textEdit.setText('')
         num = ''
+        print('eq opr => ', opr)
 
     def amaliat(self):
         global temp_num
         global num
         global opr
+        print(num, '-------', temp_num)
+        print('ama opr => ', opr)
+
+        temp_num = float("{:.2f}".format(float(temp_num)))
+        num = float("{:.2f}".format(float(num)))
+        print(num, '-------', temp_num)
         if opr == '*':
-            return(str(float(temp_num) * float(num)))
+            return float("{:.2f}".format(float(temp_num * num)))
         elif opr == '/':
-            return(str(float(temp_num) / float(num)))
+            return float("{:.2f}".format(float(temp_num / num)))
         elif opr == '-':
-            return(str(float(temp_num) - float(num)))
+            return float("{:.2f}".format(float(temp_num - num)))
         else:
-            return(str(float(temp_num) + float(num)))
+            return float("{:.2f}".format(float(temp_num + num)))
 
     def enter_clear(self):
         global temp_num
